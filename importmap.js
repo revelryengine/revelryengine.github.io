75c2e6b68f0c8cbc171aa7fa6d42af463b2851ed
Shannon Poole <shannon.m.poole@gmail.com>
2022-12-17 11:56:17 -0500

    bumped dep versions

--- importmap.js
+++ importmap.js
@@ -6,11 +6,11 @@ const element = document.createElement('script');
 element.type = 'importmap-shim';
 element.textContent = JSON.stringify({
     imports: {
-        'revelryengine/ecs/'                 : 'https://cdn.jsdelivr.net/gh/revelryengine/ecs@v0.1.0-alpha/',
-        'revelryengine/core/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/core@v0.1.0-alpha/',
-        'revelryengine/gltf/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/gltf@v0.1.0-alpha/',
-        'revelryengine/renderer/'            : 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.1.3-alpha/',
-        'revelryengine/sample-environments/' : 'https://cdn.jsdelivr.net/gh/revelryengine/sample-environments@v0.1.0-alpha/',
+        'revelryengine/ecs/'                 : 'https://cdn.jsdelivr.net/gh/revelryengine/ecs@v0.1.1-alpha/',
+        'revelryengine/core/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/core@v0.1.1-alpha/',
+        'revelryengine/gltf/'                : 'https://cdn.jsdelivr.net/gh/revelryengine/gltf@v0.1.1-alpha/',
+        'revelryengine/renderer/'            : 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.1.4-alpha/',
+        'revelryengine/sample-environments/' : 'https://cdn.jsdelivr.net/gh/revelryengine/sample-environments@v0.2.0-alpha/',
         'revelryengine/sample-models/'       : 'https://cdn.jsdelivr.net/gh/revelryengine/sample-models@v0.1.0-alpha/',
         'lit'                                : 'https://cdn.skypack.dev/lit@2.0.2',
         'lit/'                               : 'https://cdn.skypack.dev/lit@2.0.2/',
